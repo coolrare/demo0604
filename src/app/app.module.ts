d7a8875eac2daa67f02923f98850c5bc8a06c04b
Will保哥 <doggy.huang@gmail.com>
2017-06-04 16:19:54 +0800

    示範匯入 HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { MaskPipe } from './mask.pipe';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     FormsModule,
     AppRoutingModule
   ],
