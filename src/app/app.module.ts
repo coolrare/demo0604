57faf9d10e099266f3bd34075fa5fe43acb92e70
Will保哥 <doggy.huang@gmail.com>
2017-06-04 16:01:41 +0800

    建立 MaskPipe 元件
    ng g pipe mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MaskPipe } from './mask.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MaskPipe
   ],
   imports: [
     BrowserModule,
